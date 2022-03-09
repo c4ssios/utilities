589e712769687d3972ffe2ab521d81034d709b0f
c4ssios <c4ssios@gmail.com>
2022-03-09 11:12:03 +0100

    Create splitTextFile.py

---
+++ splitTextFile.py
@@ -0,0 +1,14 @@
+path = 'writePathHere'
+
+lines_per_file = 3000
+smallfile = None
+with open(path + '/' +'file.obj') as bigfile:
+    for lineno, line in enumerate(bigfile):
+        if lineno % lines_per_file == 0:
+            if smallfile:
+                smallfile.close()
+            small_filename = path + '/'+'small_file_{}.txt'.format(lineno + lines_per_file)
+            smallfile = open(small_filename, "w")
+        smallfile.write(line)
+    if smallfile:
+        smallfile.close()
